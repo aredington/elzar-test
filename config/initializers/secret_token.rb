7e3cdd8658dccba4dd74e6187562fc893b690c74
Alex Redington <alex.redington@thinkrelevance.com>
2011-12-09 15:10:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElzarTest::Application.config.secret_token = '61f6ba278d0877646fc28de287466e59c77df242f33a079e3c051f458d508209d776b27dbd21882eb6b5be1a00dd9804caff751f2f9a96f3f5e870202d57b985'
