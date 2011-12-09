81daa7704fe070f9839e808b48de6328ec93b580
Alex Redington <alex.redington@thinkrelevance.com>
2011-12-09 15:51:38 -0500

    Update to make this work with the walking skeleton...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ElzarTest::Application.config.session_store :cookie_store, key: '_elzar-test_session'
+ElzarTest::Application.config.session_store :cookie_store, :key => '_elzar-test_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
